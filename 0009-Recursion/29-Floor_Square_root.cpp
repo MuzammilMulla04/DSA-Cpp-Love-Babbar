d680d12b457e169d94578ff8e1fb44cec645e712
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:26:14 +0530

    added: .\0009-Recursion\29-Floor_Square_root.cpp

---
+++ 0009-Recursion/29-Floor_Square_root.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ifPossible(int start, int end, int n, int ans= -1) {
+    if(start >= end)
+        return ans;
+
+    int mid= start+ (end-start)/2;
+
+    if(mid*mid == n) {
+        return mid;
+    }
+    else if(mid*mid < n) {
+        ans= mid;
+        return ifPossible(mid+1, end, n, ans);
+    }
+    else
+        return ifPossible(start, mid, n, ans);
+}
+int floorSquareRoot(int n) {
+    return ifPossible(0, n-1, n);
+}
+
+int main() {
+    system("cls");
+
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Floor Square Root of " << n << " is: " << floorSquareRoot(n) << endl;
+
+    cout << endl;
+    return 0;
+}
