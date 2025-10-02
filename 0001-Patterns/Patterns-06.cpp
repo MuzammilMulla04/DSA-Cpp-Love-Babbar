c89caba3814d2a413c7942bfcc2805b4a4ae7554
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:25:02 +0530

    added: .\0001-Patterns\Patterns-06.cpp

---
+++ 0001-Patterns/Patterns-06.cpp
@@ -0,0 +1,30 @@
+/*
+Q. Print:-
+        1111
+         222
+          33
+           4
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    system("cls");
+
+    int n;
+
+    cout << "Enter Number: ";
+    cin >> n;
+
+    for (int row = 1; row <= n; row++) {
+        for (int col = 1; col <= row - 1; col++)
+            cout << " ";
+        for (int col = row; col <= n; col++)
+            cout << row;
+        cout << endl;
+    }
+
+    system("pause>0");
+    return 0;
+}
