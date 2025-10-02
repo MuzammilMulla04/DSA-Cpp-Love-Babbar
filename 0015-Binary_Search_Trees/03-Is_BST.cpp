abd79afa6c3bc0e2dbd4448e4fb623b20ec884a7
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:26:56 +0530

    added: .\0015-Binary_Search_Trees\03-Is_BST.cpp

---
+++ 0015-Binary_Search_Trees/03-Is_BST.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+bool isBST(BinaryTreeNode<int> *root, int min, int max) {
+    if(!root)
+        return true;
+
+    if(min <= root->data && root->data <= max) {
+        bool left= isBST(root->left, min, root->data);
+        bool right= isBST(root->right, root->data, max);
+
+        return left && right;
+    }
+
+    return false;
+}
+bool validateBST(BinaryTreeNode<int> *root) {
+    return isBST(root, INT_MIN, INT_MAX);
+}
