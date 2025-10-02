30e198cdc293eeff53c464900e2077f8365567b0
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:26:30 +0530

    added: .\0011-Linked_Lists\13-Split_CLL_in_2_Halves.cpp

---
+++ 0011-Linked_Lists/13-Split_CLL_in_2_Halves.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+//Constructor & Destructor
+    Node(int data) {
+        this->data= data;
+        this->next= NULL;
+    }
+    ~Node() {
+        cout << this->data << " is deleted!" << endl;
+        if(this->next != NULL) {
+            this->next= NULL;
+            delete this->next;
+        }
+    }
+//Create Node
+    Node* createNode() {
+        int data;
+        cout << "Enter data: ";
+        cin >> data;
+
+        Node* newNode= new Node(data);
+        return newNode;
+    }
+//Create Linked List(head)
+    static Node* createHead() {
+        return NULL;
+    }
+//Traversal
+    void print(Node* head) {
+        if(head  == NULL) {
+            cout << "Linked List is Empty!" << endl;
+            return;
+        }
+        cout << "Linked List: ";
+        Node* temp= head;
+        do {
+            cout << temp->data << " ";
+            temp= temp->next;
+        } while(temp != head);
+        cout << endl;
+    }
+//Insertion
+    void insertAfterValue(Node*& head) {
+        int value;
+        cout << "Enter Value: ";
+        cin >> value;
+
+        Node* newNode= createNode();
+        if(head == NULL) {
+            head=newNode;
+            newNode->next= head;
+        }
+
+        Node* temp= head;
+        while(temp->data != value && temp->next != head)
+            temp= temp->next;
+        newNode->next= temp->next;
+        temp->next= newNode;
+    }
+//Deletion
+    void deleteByValue(Node*& head) {
+// Empty List
+        if(head == NULL) {
+            cout << "Linked List is Empty!" << endl;
+            return;
+        }
+        int value;
+        cout << "Enter Value: ";
+        cin >> value;
+//Only 1 Node & found
+        if(head->next == head && head->data == value) {
+            delete head;
+            head= NULL;
+            return;
+        }
+//More than 1 Node       
+        Node* prev= head;
+        Node* temp= prev->next;
+        while(temp->data != value && temp->next != head->next) {
+            prev= temp;
+            temp= temp->next;
+        }
+    //If not Found!        
+        if(temp->data != value) {
+            cout << value << " not Found!" << endl;
+            return;
+        }        
+    //If Found
+        if(temp == head)
+            head= head->next;
+        prev->next= temp->next;
+        delete temp;
+    }
+};
+
+void splitInTwoHalves(Node* head, Node*& first, Node*& second) {
+    first= head;
+    second= head;
+
+    do {
+        first= first->next;
+        if(first != head)
+            first= first->next;
+
+        second= second->next;
+    } while(first != head);
+
+    head= first;
+    while(head->next != second)
+        head= head->next;
+    head->next= first;
+
+    head= second;
+    while(head->next != first)
+        head= head->next;
+    head->next= second;
+}
+
+int main() {
+    system("cls");
+
+    int n;
+    cout << "Enter Size for Linked List: ";
+    cin >> n;
+
+    Node* head = Node::createHead();
+    while (n--)
+        head->insertAfterValue(head);
+    head->print(head);
+
+    Node* first= NULL, *second= NULL;
+    splitInTwoHalves(head, first, second);
+    
+    first->print(first);
+    second->print(second);
+
+    cout << endl;
+    return 0;
+}
