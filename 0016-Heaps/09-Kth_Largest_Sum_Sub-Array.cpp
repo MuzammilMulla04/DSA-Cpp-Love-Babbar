b7adec420af7a377a5bb7f337280e4cabc73424c
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:27:06 +0530

    added: .\0016-Heaps\09-Kth_Largest_Sum_Sub-Array.cpp

---
+++ 0016-Heaps/09-Kth_Largest_Sum_Sub-Array.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getKthLargest(vector<int> &arr, int k) {
+    int n= arr.size();
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    for(int i=0; i<n; i++) {
+        int sum= 0;
+        for(int j=i; j<n; j++) {
+            sum += arr[j];
+            
+            if(minHeap.size() < k) {
+                minHeap.push(sum);
+            } else {
+                if(minHeap.top() < sum) {
+                    minHeap.pop();
+                    minHeap.push(sum);
+                }
+            }
+        }
+    }
+
+    return minHeap.top();
+}
