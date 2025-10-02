4751615a955a63539485ea02c14cbaa4631c06a1
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:26:59 +0530

    added: .\0015-Binary_Search_Trees\09-Flatten_To_Doubly.cpp

---
+++ 0015-Binary_Search_Trees/09-Flatten_To_Doubly.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    TreeNode<T>* left;
+    TreeNode<T>* right;
+
+    TreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~TreeNode() {
+        if (left)
+            delete left;
+        if (right)
+            delete right;
+    }
+};
+
+void makelist(TreeNode<int>* root, TreeNode<int>*& head) {
+    if (!root)
+        return;
+
+    makelist(root->right, head);
+
+    root->right = head;
+    if(head)
+        head->left= root;
+    head = root;
+
+    makelist(root->left, head);
+
+    return;
+}
+TreeNode<int>* flatten(TreeNode<int>* root) {
+    TreeNode<int>* head = NULL;
+    makelist(root, head);
+    return head;
+}
