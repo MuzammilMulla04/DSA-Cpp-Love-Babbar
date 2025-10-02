c4c37cb214b734d237d31a64f0e1c192a17fce7a
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:26:49 +0530

    added: .\0014-Binary_Trees\14-Left_View.cpp

---
+++ 0014-Binary_Trees/14-Left_View.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+class Solution {
+    void solve(Node* root, int level, vector<int>& ans) {
+        if(!root)
+            return;
+        
+        if(level == ans.size()) {
+            ans.push_back(root->data);
+        }
+
+        solve(root->left, level+1, ans);
+        solve(root->right, level+1, ans);
+    }
+public:
+    vector<int> leftView(Node *root) {
+        vector<int> ans;
+        solve(root, 0, ans);
+        return ans;
+    }
+};
