992cef6479551058ae4eccf453e0758f5ae1b43f
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:27:11 +0530

    added: .\0018-Trie\05-Implement_Trie-2.cpp

---
+++ 0018-Trie/05-Implement_Trie-2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TrieNode {
+public:
+    char data;
+    vector<TrieNode*> children;
+    bool isTerminal;
+
+    TrieNode():  {}
+};
+
+class Trie {
+public:
+    Trie() {
+        // Write your code here.
+    }
+
+    void insert(string &word) {
+        // Write your code here.
+    }
+
+    int countWordsEqualTo(string &word) {
+        // Write your code here.
+    }
+
+    int countWordsStartingWith(string &word) {
+        // Write your code here.
+    }
+
+    void erase(string &word) {
+        // Write your code here.
+    }
+};
