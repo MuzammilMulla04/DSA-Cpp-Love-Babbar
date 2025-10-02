33bb4f7a381c97d10cafcd9f2410ca48ba2bfbda
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:26:47 +0530

    added: .\0014-Binary_Trees\12-Top_View.cpp

---
+++ 0014-Binary_Trees/12-Top_View.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+class Solution {
+public:
+    vector<int> topView(Node *root) {
+        vector<int> ans;
+        if(!root)
+            return ans;
+
+        map<int, int> mapping;
+        queue<pair<Node*, int>> q;
+
+        q.push(make_pair(root, 0));
+
+        while(!q.empty()) {
+            pair<Node*, int> temp= q.front();
+            q.pop();
+
+            Node* frontNode= temp.first;
+            int distance= temp.second;
+
+            int distance= temp.second;
+
+            if(!mapping[distance])
+                mapping[distance]= (frontNode->data);
+
+            if(frontNode->left)
+                q.push(make_pair(frontNode->left, distance-1));
+            if(frontNode->right)
+                q.push(make_pair(frontNode->right, distance+1));
+        }
+
+        for(auto map: mapping) {
+            ans.push_back(map.second);
+        }
+
+        return ans;
+    }
+};
