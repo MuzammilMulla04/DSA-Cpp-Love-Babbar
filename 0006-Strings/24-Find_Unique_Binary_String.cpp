38ed78a739ac1e6764dba428543ccbdd1822464a
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:25:48 +0530

    added: .\0006-Strings\24-Find_Unique_Binary_String.cpp

---
+++ 0006-Strings/24-Find_Unique_Binary_String.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        string findDifferentBinaryString(vector<string>& nums) {
+        int n= nums.size();
+        string ans= "";
+    
+            for(int i=0; i<n; ++i)
+                ans += nums[i][i] == '1'? '0': '1';
+    
+            return ans;
+        }
+    };
