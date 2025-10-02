dbdc30fc5354d32a8cc1891b78346a06b5c87c3e
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:25:40 +0530

    added: .\0006-Strings\01-Char_Arrays.cpp

---
+++ 0006-Strings/01-Char_Arrays.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    system("cls");
+
+    char name[20];
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Your name is: " << name << endl;
+
+    cout << endl;
+    return 0;
+}
