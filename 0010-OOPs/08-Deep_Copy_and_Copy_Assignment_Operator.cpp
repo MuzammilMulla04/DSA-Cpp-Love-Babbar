ab286fc600152688fe72d867a29932cf3aee75fe
Muzammil Altaf Mulla <146463412+MuzammilMulla04@users.noreply.github.com>
2025-10-02 13:26:22 +0530

    added: .\0010-OOPs\08-Deep_Copy_and_Copy_Assignment_Operator.cpp

---
+++ 0010-OOPs/08-Deep_Copy_and_Copy_Assignment_Operator.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class hero {
+private:
+    int health;
+public:
+    char *name;
+    char level;
+//Deep-Copy Constructor
+    hero(hero& temp) {
+        char* ch= new char[strlen(temp.name)+1];        //Creating a new array
+        strcpy(ch, temp.name);
+        this->name= ch;          //Shallow Copy- Same Memory allocated, of pointer-type data member, to copy Object
+        this->health= temp.health;      //Deep Copy- Different Memory allocated to copy Object
+        this->level= temp.level;
+    }
+//Simple Constructor
+    hero() {
+        name= new char[100];        //Allocating memory dynamically to creation of object
+    }
+//Parameterized Constructor
+    hero(int health, char level) {
+        this->health= health;
+        this->level= level;
+    }
+// Getters & Setters
+    int getHealth() {
+        return health;
+    }
+    char getLevel() {
+        return level;
+    }
+    void setName(char name[]) {
+        strcpy(this->name, name);
+    }
+    void setHealth(int Health) {
+        health= Health;
+    }
+    void setLevel(char Level) {
+        level= Level;
+    }
+// Other Functions
+    void print() {
+        cout << endl << "[" << name << ", " << getHealth()  << ", " << getLevel() << "]" << endl;
+    }
+};
+
+int main() {
+    system("cls");
+
+    hero h1;
+    h1.setHealth(100);
+    h1.setLevel('A');
+    char name[]= "Muzammil";
+    h1.setName(name);
+    h1.print();
+
+/*
+    hero h2(h1);    //OR
+
+    hero h2;
+    h2= h1;         //Using copy Assignment operator
+ */    
+    hero h2= h1;        //Another Way!
+    
+    h2.print();
+
+    h1.name[0]= 'L';
+    h1.print();
+    h2.print();     //Now it won't change!
+
+    cout << endl;
+    return 0;
+}
